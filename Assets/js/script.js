219020ff2281802b25b74697fa2fc11478f6008d
bmalbright <brianalbright@utexas.edu>
2021-10-10 14:34:52 -0500

    api's are working. getting good returns to the page for current weather and forecast.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,80 @@
+
+var part = "minutely,hourly";
 var apiKey = "afd682401c0cee2cf220ad5a92b3a135";
-var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
-var fiveDay = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
+var saveBtn = document.querySelector(".saveBtn");
 
-var lon = -94.04
-var lat = 33.44
-var city
+saveBtn.addEventListener("click", function () {
+  var cityName = document.querySelector(".city").value;
+  
+  console.log(cityName);
+  gettingCurrentWeather(cityName);
+  localStorage.setItem(cityName);  
+});
 
-function oneCallApi() {
+var weather = document.querySelector(".currentWeather");
 
-    $.ajax({
-      url: oneCall,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response)
+var gettingCurrentWeather = function (city) {
+  var currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  fetch(currentWeather).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        var tempTag = document.createElement("p");
+        tempTag.textContent = "Temperature: " + data.main.temp;
+        var humidityTag = document.createElement("p");
+        humidityTag.textContent = "Humidity: " + data.main.humidity + "%";
+        var windSpeedTag = document.createElement("p");
+        windSpeedTag.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        weather.append(tempTag, humidityTag, windSpeedTag);
+        oneCall(lat, lon);
+      });
+    } else {
+      alert(`Error: ${response.statusText}`);
     }
+  });
+};
+
+var oneCall = function (lat, lon) {
+  var oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}&units=imperial`;
+  fetch(oneCallAPI).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+
+        var uvIndexTag = document.createElement("p");
+        uvIndexTag.textContent = "UV Index: " + data.current.uvi 
+        weather.append(uvIndexTag)
+        var slicedArray = data.daily.slice(0,5);
+
+        var forecast = document.querySelector(".fiveDayForecast");
+
+        for (var i = 0; i < slicedArray.length; i++) {
+           var element = slicedArray[i];
+           console.log(element)
+          var card = document.createElement("div");
+          card.setAttribute("class", "card");
+          var cardBody = document.createElement("div");
+          cardBody.setAttribute("class", "card-body")
+          
+
+          var tempTag = document.createElement("p");
+          tempTag.textContent = "Temperature: " + element.temp.max;
+          var windSpeedTag = document.createElement("p");
+          windSpeedTag.textContent = "Wind Speed: " + element.wind_speed + " MPH";
+          var humidityTag = document.createElement("p");
+          humidityTag.textContent = "Humidity: " + element.humidity + "%";
+
+          cardBody.append(tempTag, windSpeedTag, humidityTag)
+          card.append(cardBody)
+          forecast.append(card);
+        }
+
+      });
+    } else {
+      alert(`Error: ${response.statusText}`);
+    }
+  });
+
+
+};
